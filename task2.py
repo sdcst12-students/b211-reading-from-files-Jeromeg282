987453feb2de05782b852bc73d810f7e97175817
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-10-21 21:55:52 -0700

    upd

--- task2.py
+++ task2.py
@@ -4,3 +4,29 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
+
+def is_pythagorean_triple(a, b, c):
+    return a**2 + b**2 == c**2
+
+def pythagoreantriplesin_file(filename):
+    count = 0
+    
+    with open(filename, 'r') as file:
+        for line in file:
+           
+            numbers = line.strip().split()
+            if len(numbers) == 3:
+                try:
+                    a, b, c = map(int, numbers)
+                    
+                    if is_pythagorean_triple(2*a, 2*b, 2*c):
+                        count += 1
+                except ValueError:
+                    pass
+
+    return count
+
+filename = "task02a.txt"  
+pythagorean_triples_count = pythagoreantriplesin_file(filename)
+
+print("Number of Pythagorean triples in the file:", pythagorean_triples_count)
