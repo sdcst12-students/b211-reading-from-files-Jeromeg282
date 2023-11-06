293c795bf0b72f6c6e8207022086853de092bd44
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-11-05 23:04:03 -0800

    restart

--- task2.py
+++ task2.py
@@ -5,19 +5,5 @@ Pythagorean triples are numbers where all of the sides are integers, and the 3 s
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
 
-def is_pythagorean_triple(a, b, c):
-    return (a / 2)**2 + (b / 2)**2 == (c / 2)**2
-
-def pythagoreandouble(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            numbers=line.strip().split
 
 
-
-
-
-filename = "task02a.txt"
-pythagorean_triples_count = pythagoreantriplesin_file(filename)
-
-print("Number of Pythagorean triples in the file:", pythagorean_triples_count)
