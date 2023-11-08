759e11628418835bed03846c01b4dcdf36ffe5df
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-11-08 13:27:20 -0800

    .

--- task2.py
+++ task2.py
@@ -5,5 +5,20 @@ Pythagorean triples are numbers where all of the sides are integers, and the 3 s
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
 
+def pythag_trip():
+    task02a='task02a.txt'
+    file = open(task02a,'r')
+    data=file.read()
+    datalist=data.split('n\n')
 
+    for i in datalist:
+        listsplit=i.split('n\n')
+        listsplit.sort()
+        print(listsplit)
+        try:
+            if ((float(listsplit[0])**2)) + ((float(listsplit[1]))**2) - ((float(listsplit[2])**2)):
+                print(datalist)
+        except:
+            pass
 
+pythag_trip()
